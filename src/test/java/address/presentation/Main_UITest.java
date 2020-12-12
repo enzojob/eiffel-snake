5b855e7b39b03de204b250905f6f21f725e3a3a1
Theo Rey <reytheopro@gmail.com>
2020-12-12 17:42:48 +0100

    src test folder created

---
+++ src/test/java/address/presentation/Main_UITest.java
@@ -0,0 +1,264 @@
+/**
+ * 
+ */
+package address.presentation;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author reyth
+ *
+ */
+public class Main_UITest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link address.presentation.Main_UI#start(javafx.stage.Stage)}.
+	 */
+	@Test
+	public void testStartStage() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link address.presentation.Main_UI#setDirection(address.model.Snake, int)}.
+	 */
+	@Test
+	public void testSetDirection() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link address.presentation.Main_UI#setSpeed()}.
+	 */
+	@Test
+	public void testSetSpeed() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link address.presentation.Main_UI#main(java.lang.String[])}.
+	 */
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link javafx.application.Application#launch(java.lang.Class, java.lang.String[])}.
+	 */
+	@Test
+	public void testLaunchClassOfQextendsApplicationStringArray() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link javafx.application.Application#launch(java.lang.String[])}.
+	 */
+	@Test
+	public void testLaunchStringArray() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link javafx.application.Application#Application()}.
+	 */
+	@Test
+	public void testApplication() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link javafx.application.Application#init()}.
+	 */
+	@Test
+	public void testInit() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link javafx.application.Application#start(javafx.stage.Stage)}.
+	 */
+	@Test
+	public void testStartStage1() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link javafx.application.Application#stop()}.
+	 */
+	@Test
+	public void testStop() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link javafx.application.Application#getHostServices()}.
+	 */
+	@Test
+	public void testGetHostServices() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link javafx.application.Application#getParameters()}.
+	 */
+	@Test
+	public void testGetParameters() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link javafx.application.Application#notifyPreloader(javafx.application.Preloader.PreloaderNotification)}.
+	 */
+	@Test
+	public void testNotifyPreloader() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link javafx.application.Application#getUserAgentStylesheet()}.
+	 */
+	@Test
+	public void testGetUserAgentStylesheet() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link javafx.application.Application#setUserAgentStylesheet(java.lang.String)}.
+	 */
+	@Test
+	public void testSetUserAgentStylesheet() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#Object()}.
+	 */
+	@Test
+	public void testObject() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#getClass()}.
+	 */
+	@Test
+	public void testGetClass() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#hashCode()}.
+	 */
+	@Test
+	public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#equals(java.lang.Object)}.
+	 */
+	@Test
+	public void testEquals() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#clone()}.
+	 */
+	@Test
+	public void testClone() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#toString()}.
+	 */
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#notify()}.
+	 */
+	@Test
+	public void testNotify() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#notifyAll()}.
+	 */
+	@Test
+	public void testNotifyAll() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#wait(long)}.
+	 */
+	@Test
+	public void testWaitLong() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#wait(long, int)}.
+	 */
+	@Test
+	public void testWaitLongInt() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#wait()}.
+	 */
+	@Test
+	public void testWait() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#finalize()}.
+	 */
+	@Test
+	public void testFinalize() {
+		fail("Not yet implemented");
+	}
+
+}
